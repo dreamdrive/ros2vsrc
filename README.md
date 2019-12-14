0fc7586608bda0af7e6c433da87c8410936df630
Hirokazu Onomichi <hirokazu@dream-drive.net>
2019-12-14 16:14:46 +0900

    ver1.1

--- README.md
+++ README.md
@@ -3,27 +3,24 @@ VS-RC003(Vstone社製ロボット用制御ボード)をシリアルポート経�
 
 ![ros2vsrc](http://dream-drive.net/images/ros2vsrc.jpg "ros2vsrc")
 
-RaspberryPiにROS kineticをインストールした環境で開発しています。
-ROSワークスペースのsrcフォルダ内に置けば、catkin_makeでコンパイル出来ます。
-おそらく、Ubuntuが動けばデスクトップPCでも、USBシリアルケーブルで同様に動作させることが可能と思います。
 
-20190728更新 : vsrc_connect2とvsrc_control2を追加
+20190728更新 : vsrc_connect2とvsrc_control2を追加  
+20191214更新 : vsrc_connect2とvsrc_control2を、vsrc_connectとvsrc_controlに変更。従来のvsrc_connectとvsrc_controlはリタイア(.old)。
 
 MIT License / Copyright (c) 2019 Hirokazu Onomichi
 
-## vsrc_connect2 (ノード)
-VS-RC003を制御するためのノードです。本ノードで、VS-RC003とシリアル通信を行います。
+## vsrc_connect (ノード)
+VS-RC003を制御するためのノードです。
 
-* カスタムメッセージVsrcControlをSubscribします。VsrcControlはコントローラーのボタンの状態とトルクON/OFFの情報で構成され、Subscribした情報をもとにVS-RC003のメモリを書き換えます。
-* VS-RC003の状態としてボードのメモリ上の情報を定期的にカスタムメッセージVsrcStateとBatteryStateへPublishします。接続先のノードから電源電圧やモーションの再生状況を監視するすることが出来ます。メッセージの詳しい内容は後記もしくは、msgファイルを参照してください。
-(従来、旧vsrc_connectでは、VSRCコマンドそのものを文字列として飛ばしていましたが、必要なものを小分けにしてメッセ―ジ化しました。)
+* カスタムメッセージVsrcControlをSubscribします。  
+VsrcControlはコントローラーのボタンの状態とトルクON/OFFの情報で構成され、Subscribした情報をもとにVS-RC003のメモリを書き換えます。
+* VS-RC003の状態としてボードのメモリ上の情報を定期的にカスタムメッセージVsrcStateとBatteryStateへPublishします。  
+接続先のノードから電源電圧やモーションの再生状況を監視するすることが出来ます。  
+メッセージの詳しい内容は後記もしくは、msgファイルを参照してください。
 
-また、テスト動作としては、rqt_topic_monitorおよびrqt_ez_publisherだけでも、全機能を試すことが出来ます。
-
-## vsrc_control2 (ノード)
-joy_nodeからジョイスティックのトピック(/joy)をsubscribeし、VsrcControlのメッセージをpublishするサンプルノードです。
+## vsrc_control (サンプルノード)
+公式joyパッケージのjoy_nodeからジョイスティックのトピック(/joy)をsubscribeし、VsrcControlのメッセージをpublishするサンプルノードです。
 Xbox360のコントローラを例に作成しています。
-(従来、旧vsrc_controlでは、このノードの中でVSRCコマンドを生成していましたが、今回はVsrcControlメッセージを利用するため、互換性はありません。)
 
 ## VsrcState.msgとVsrcControl.msg (メッセージ)
 VsrcStateとVsrcControl、2つのカスタムメッセージを用意しました。
@@ -33,44 +30,48 @@ VsrcStateはVS-RC003の状態を表すメッセージ、VsrcControlはVS-RC003�
 ![ROS_TOPIC](http://dream-drive.net/images/ros_topic_vsrc.png "メッセージの中身")
 
 ## 旧vsrc_connectノードと旧vsrc_controlノードについて
-旧vsrc_controlノードとvsrc_connectも残していますが、互換性はありません。
-VSRCの専用コマンドを文字列として送っていたため、専用コマンドを覚えないと使えないという欠点があったため、使用は推奨しません。
+互換性がありません。
 
-# 使用例
-RaspberryPiに接続されたVS-RC003をLANを経由して、遠隔のワークステーションからコントロールします。
-(RaspberryPiにXBOXコントローラを繋いで、localで実行しても構いません。)
+# 使用例1 - PCにVS-RC003もジョイスティックも直接接続する場合
+
+PCに接続した、XBOX360コントローラーをVS-RC003のコントローラーとして使用するサンプルlaunchです。シリアルポートのデバイス名をパラメータ"serialdev"にセットして起動してください。
+
+```
+$ roslaunch ros2vsrc joystick2vsrc.launch serialdev:=/dev/ttyUSB0
+```
+
+
+# 使用例2 - VS-RC003を接続したRaspberryPiを、ネットワーク越しにPCに接続したジョイスティックから操作する
 
 ## 事前準備
 RaspberryPiのGPIOを、VS-RC003のCN6もしくはCN7のTxD,RxD,GNDに、それぞれ直結します。
-PCに繋ぐ場合は、USBシリアルケーブルをUARTのレベルに変換して接続してください。
 
-ROSMASTERは、どちら側に設定しても構いませんが、
 ```
-export ROS_MASTER_URI=http://(roscoreが動いているIP):11311/
+export ROS_MASTER_URI=http://(RaspberryPiのIP):11311/
 export ROS_IP = (自分のIP)
 ```
 を、ノード起動前に、両側で正しく実行すること。
-(ローカルでjoy_nodeも立ち上げる場合は設定不要です。)
+
+※ ROS MASTERをRaspberryPi側に設定する例です。  
+※ 自分のIPとは、RaspPiはRasPiのIP、PCはPCのIPです。
 
 ## 実行方法
-vsrc_connect2ノードをRaspberryPi上で実行します。
+vsrc_connectノードをRaspberryPi上で実行します。
 ```
-rosrun ros2vsrc vsrc_connect2
+$ roscore &
+$ rosrun ros2vsrc vsrc_connect _serialdev:=/dev/ttyAMA0
 ```
+※ 「/dev/ttyAMA0」は、RaspberryPiのGPIOのシリアルポート名です。
 
-ワークステーション側にXBOX360のコントローラを接続し、joy_nodeノードを起動します。
+PC側にXBOX360のコントローラを接続し、joy_nodeノードとvsrc_controlノードを起動します。
 ```
-rosrun joy joy_node
-```
-
-同じくワークステーション上でvsrc_controlノードを立ち上げます。
-```
-rosrun ros2vsrc vsrc_control2
+$ rosrun joy joy_node &
+$ rosrun ros2vsrc vsrc_control
 ```
 起動すると、rqt_graphの様子はこんな感じになります。
 ![ROS_NODE](http://dream-drive.net/images/ros_node_vsrc.png "ノードの状態")
 
-これで、LAN/WiFi経由でROSを使ってVS-RC003の制御出来ます。
+これで、LAN/WiFi経由でROSを使ってVS-RC003の制御出来ます。  
 また、ROS側で動作計画を立ててVS-RC003にモーションの指令を出す、といったVS-RC003だけでは実行できないより高度な自律動作をさせることも出来るかもしれません。
 
 # 謝辞
--- src/vsrc_connect.cpp
+++ src/vsrc_connect.cpp
@@ -1,186 +1,526 @@
-// vsrc_connect (テストバージョンのため、今後はvsrc_connect2を使用することを推奨します)
+﻿// vsrc_connect
+// VS-RC003を制御するノード
 //
-// VS-RC003用のコマンドをvsrc_controlからsubscribeし、シリアルポートを通じてVS-RC003と通信するノード
+// シリアルポートのデバイス名は "/dev/ttyUSB0" でシリアルポートを3.3V系の信号に変換し、VS-RC003のCN6もしくはCN7のTxD,RxD,GNDと接続して使用してください
+// RaspberryPiの場合は、GPIOが3.3V系のためVS-RC003のCN6もしくはCN7のTxD,RxD,GNDと、それぞれ直結して使用する
 // 
-// RaspberryPiで実行されることを想定して作っています。
-// デバイス名は "/dev/ttyAMA0" で、RaspberryPiのGPIOをVS-RC003のCN6もしくはCN7のTxD,RxD,GNDと、それぞれ直結して使用する
-// PCからUSBシリアルでも使えますが、適宜デバイス名を変更するのと、VS-RC003のUARTに合わせてレベル変換してください。
-// 
-// (注意!!) コマンドは改行コードなしの正味のコマンド部分のみで、改行コードはシリアル送信時にこのノード内で付与します。
-//
-// (使い方例1 : サーボトルクON)
-// rostopic pub /vsrc_write std_msgs/String "w 2009f6 01 00"
-// (使い方例2 : △ボタン押下)
-// rostopic pub /vsrc_write std_msgs/String "w 2009e2 10 00"
-// (使い方例3 : △ボタン解放)
-// rostopic pub /vsrc_write std_msgs/String "w 2009e2 00 00"
-// (使い方例4 : サーボトルクOFF)
-// rostopic pub /vsrc_write std_msgs/String "w 2009f6 00 00"
-// 
-// こちらの記事を参考にさせていただきました。ありがとうございます。
-// https://qiita.com/srs/items/efaa8dc0a6d580c7c423
-// https://github.com/project-srs/ros_lecture/blob/master/hard_lecture/src/hard_serialport_retry.cpp
-//
-// 20190727 : bug fix (シリアル通信の改行コードの設定について)
+// シリアルポートの名称が変わる場合は、下記のようにプライベートパラメータにシリアルポートを指定して起動してください。(RaspberryPiの場合)
+// $ rosrun ros2vsrc vsrc_connect _serialdev:=/dev/ttyAMA0
 //
 // MIT License
 // Copyright (c) 2019 Hirokazu Onomichi
 
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "diagnostic_updater/diagnostic_updater.h"
+
+#include "sensor_msgs/BatteryState.h"
+#include "ros2vsrc/VsrcState.h"
+#include "ros2vsrc/VsrcControl.h"
 
 #include <string>
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
 
-#define SERIAL_PORT "/dev/ttyAMA0"	// RaspberryPiのシリアルポート デバイス名
+#define BUFF_SZ (256)				// 送受信バッファのサイズ
+#define SERIAL_PORT "/dev/ttyUSB0"	// デフォルトのシリアルポート デバイス名
 
-class serial_stream{
-private:
-    int fd;
-    std::string device_name;
-    bool connected;
-    int read_success;
-public:
-    serial_stream(void){
-        connected=false;
-        read_success=0;
-    }
-    void set_name(std::string name){
-        device_name=name;
+#define FALSE 0
+#define TRUE 1
+#define DEBUG FALSE
 
-    }
-    void ss_open(void){
-        fd=open(device_name.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
-        fcntl(fd, F_SETFL,0);
-        //load configuration
-        struct termios conf_tio;
-        tcgetattr(fd,&conf_tio);
-        //set baudrate : VS-RC003 = ボーレート115200bps
-        speed_t BAUDRATE = B115200;
-        cfsetispeed(&conf_tio, BAUDRATE);
-        cfsetospeed(&conf_tio, BAUDRATE);
-        //non canonical, non echo back
-        conf_tio.c_lflag &= ~(ECHO | ICANON);
-    	// inputの設定
-        conf_tio.c_iflag &= IGNCR;	// 受信したIGNCR (CRLF)のCRを無視する(そのまま受信する) !!!
-		// outputの設定
-        conf_tio.c_oflag &= ~ONLCR;	// 送信時LFをCRLFに変換する機能:ONLCRを反転(OFF)にする !!!
-        //non blocking
-        conf_tio.c_cc[VMIN]=0;
-        conf_tio.c_cc[VTIME]=0;
-        //store configuration
-        tcsetattr(fd,TCSANOW,&conf_tio);
-        if(fd>=0){
-            connected=true;
-        }
-        else{
-            connected=false;
-        }
-    }
-    void ss_write(std::string data0){
-        if(connected){
-            int rec=write(fd,data0.c_str(),data0.size());
-            if(rec<0){
-                connected=false;
-                ss_close();
-            }
-        }
-    }
-    std::string ss_read(void){
-        if(connected){
-            char buf[256]={0};
-            int recv_data=read(fd, buf, sizeof(buf));
-            if(recv_data>0){
-                read_success++;
-                std::string recv_string=buf;
-                return buf;
-            }
-            else{
-                return "";
-            }
-        }
-    }
-    void ss_close(void){
-        close(fd);
-        connected=false;
-    }
-    bool ss_connected(void){
-        return connected;
-    }
-    bool ss_status(void){
-        if(read_success>0){
-            read_success=0;
-            return true;
-        }
-        else return false;
-    }
-};
-diagnostic_updater::Updater *p_updater;
-ros::Publisher serial_pub;
-std::string device_name = SERIAL_PORT;
-serial_stream ss0;
+#define WAIT_TIME 20000	// (usec) シリアルポートに対するコンピュータの処理能力によって調整(raspiは0、core i7では大きめの値に要調整必要)
 
-void serial_callback(const std_msgs::String& serial_msg){
-    ss0.ss_write(serial_msg.data);
-    ss0.ss_write("\r\n");				// VS-RC003の改行コードをつける
-}
-bool first_time=true;
-bool last_connected=false;
-void timer_callback(const ros::TimerEvent&){
-        if(!ss0.ss_connected()){
-            ss0.ss_open();
-            if(ss0.ss_connected())ROS_INFO("Serial Open %s", device_name.c_str());
-            else{
-                if     (first_time)    ROS_ERROR("Serial Fail: cound not open %s", device_name.c_str());
-                else if(last_connected)ROS_ERROR("Serial Fail: Connection is broken %s", device_name.c_str());
-            }
-        }
-        else{
-            std::string recv_data=ss0.ss_read();
-            if(recv_data.size()>0){
-                std_msgs::String serial_msg;
-                serial_msg.data=recv_data;
-                serial_pub.publish(serial_msg);
-            }
-        }
-        first_time=false;
-        last_connected=ss0.ss_connected();
-        p_updater->update();        
-        ros::spinOnce();
+int fd1;			// シリアルポート
+
+int r_voltage;		// 電圧
+int r_mode;			// モード
+int r_map;			// マップ
+int r_ctltype;		// コントロールの種類(未接続=0、ゲームパッド=1、ProBo=2、RRC-T11=3)
+int r_torque;		// トルクON/OFF(読み取り)
+int r_idle;			// アイドル状態
+
+bool w_torque = 0;	// トルクON/OFF(書き込み)
+bool b_left = 0;
+bool b_down = 0;
+bool b_right = 0;
+bool b_up = 0;
+bool b_start = 0;
+bool b_R3 = 0;
+bool b_L3 = 0;
+bool b_select = 0;
+bool b_square = 0;
+bool b_cross = 0;
+bool b_circle = 0;
+bool b_triangle = 0;
+bool b_R1 = 0;
+bool b_L1 = 0;
+bool b_R2 = 0;
+bool b_L2 = 0;
+short s_lx = 0;
+short s_ly = 0;
+short s_rx = 0;
+short s_ry = 0;
+
+int open_serial(const char *device_name){
+	int fd1 = open(device_name, O_RDWR | O_NOCTTY | O_NONBLOCK);
+	fcntl(fd1, F_SETFL,0);
+	//load configuration
+	struct termios conf_tio;
+	tcgetattr(fd1,&conf_tio);
+	//set baudrate : VS-RC003 = ボーレート115200bps
+	speed_t BAUDRATE = B115200;
+	cfsetispeed(&conf_tio, BAUDRATE);
+	cfsetospeed(&conf_tio, BAUDRATE);
+	//non canonical, non echo back
+	conf_tio.c_lflag &= ~( ECHO | ICANON );
+	// inputの設定
+	conf_tio.c_iflag &= IGNCR;	// 受信したIGNCR (CRLF)のCRを無視する(そのまま受信する) (重要)
+	// outputの設定
+	conf_tio.c_oflag &= ~ONLCR;	// 送信時LFをCRLFに変換する機能:ONLCRを反転(OFF)にする (重要)
+	//non blocking
+	conf_tio.c_cc[VMIN]=0;
+	conf_tio.c_cc[VTIME]=0;
+	//store configuration
+	tcsetattr(fd1,TCSANOW,&conf_tio);
+	return fd1;
 }
 
-void diagnostic0(diagnostic_updater::DiagnosticStatusWrapper &stat){
-    bool serial_c=ss0.ss_connected();
-    bool serial_s=ss0.ss_status();
-    if     (serial_c &&  serial_s)stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK,    "Active.");
-    else if(serial_c && !serial_s)stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,  "No Recieve.");
-    else                          stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "No Connection.");
+////////////////////////////////////////////////////////////////
+//
+// debug_print
+// 送受信したパケットを2行に表示（改行コードを'R'と'N'に置換)
+//
+////////////////////////////////////////////////////////////////
+void debug_print(char *wbuf, char *rbuf){
+	char wbuf_d[BUFF_SZ],rbuf_d[BUFF_SZ];
+	int i;
+
+	//表示用に整形
+	for (i = 0; i<BUFF_SZ; i++) {
+		wbuf_d[i] = wbuf[i];	// デバッグ用
+		if (wbuf[i] == '\n') wbuf_d[i]='N';	// デバッグ用
+		if (wbuf[i] == '\r') wbuf_d[i]='R';	// デバッグ用
+	}
+
+	//表示用に整形
+	for (i = 0; i<BUFF_SZ; i++) {
+		rbuf_d[i] = rbuf[i];	// デバッグ用
+		if (rbuf[i] == '\n') rbuf_d[i]='N';	// デバッグ用
+		if (rbuf[i] == '\r') rbuf_d[i]='R';	// デバッグ用
+	}
+
+	ROS_INFO("TX(%03d) >> %s",(int)strlen(wbuf),wbuf_d);
+	ROS_INFO("RX(%03d) << %s",(int)strlen(rbuf),rbuf_d);
+}
+
+////////////////////////////////////////////////////////////////
+//	sendmessage関数
+//	メッセージを送受信する関数。
+//	
+//	・引数
+//		char *wbuf	送信メッセージバッファへのポインタ
+//		char *rbuf	受信メッセージバッファへのポインタ
+////////////////////////////////////////////////////////////////
+
+int sendmessage(char *wbuf, char *rbuf){
+
+	bool werr(FALSE),rerr(FALSE);
+	int i = 0, cr_num = 0 , flag = 0 , j = 0;
+
+	if(!write(fd1,wbuf,(int)strlen(wbuf))) werr = TRUE;	// VS-RC003へ送信 / 書き込みができなければエラーフラグをたてる
+
+	//メッセージの受信
+	//メッセージの最後まで1byteずつ読み込みを行なう
+	//メッセージの最後の判断は、2回目に改行('\n')が登場する場所。1回目は送信メッセージ、2回目はCPUボードが付記するもの
+	//また、受信がタイムアウトを迎えた場合もループを抜ける
+	
+	usleep(WAIT_TIME);		// 送受信切替時のウェイト環境依存
+
+	while (1) {	
+		flag = read(fd1, &rbuf[i], 1);	// 1文字受信(返り値は受信バイト数(1):受信バッファが空なら(0) 
+		if(flag > 0){	//受信した時
+			if(rbuf[i] == 0x0a) cr_num++;	//その文字が改行コードなら改行カウントアップ
+			i++; 							//次の文字へ
+		}
+		else{		//受信しなかったとき
+			j++; 							// タイムアウト用のカウントアップ
+		}
+
+		if ((rbuf[0]=='w') && (cr_num >= 1)) break;	//wコマンドの時、改行コードが1つ来ると無限ループブレイク
+		if ((rbuf[0]=='r') && (cr_num >= 2)) break;	//rコマンドの時、改行コードが2つ来ると無限ループブレイク
+
+		if (j >= 10000) {			// 10000回繰り返すとタイムアウト終了
+			rerr = TRUE;			// 受信エラーフラグ
+			break;
+		}
+	}
+
+	//シリアルポート受信バッファクリア 改行コードでbreakしているのでバッファに残っているハズのNullをクリア 
+	tcflush(fd1,TCIFLUSH);
+
+	if(werr) ROS_WARN("WRITE ERROR (sendmessage)");
+	if(rerr) ROS_WARN("READ ERROR - time out (sendmessage)");
+
+	return (werr<<1) | rerr ;
+}
+
+////////////////////////////////////////////////////////////////
+//	get_memmap8関数
+//	メモリマップのアドレスを指定して、8アドレス分の値を読む関数
+//	
+//	・引数
+//		unsigned char map_add 読み出したいメモリマップのアドレス0～255
+//		short value[] 値を返すための配列変数8個分
+//
+//	・戻り値
+//		正常終了は0、異常終了は-1
+////////////////////////////////////////////////////////////////
+
+int get_memmap8(unsigned char map_add , short value[]) {
+	char rbuf[BUFF_SZ], wbuf[BUFF_SZ], rbuf2[BUFF_SZ];
+	char outputc0[5],outputc1[5],outputc2[5],outputc3[5],outputc4[5],outputc5[5],outputc6[5],outputc7[5];
+	int pos_sharp = 0, i ,length;
+
+	//送受信メッセージバッファのクリア
+	memset(wbuf, 0x00, sizeof(wbuf));
+	memset(rbuf, 0x00, sizeof(rbuf));
+	memset(rbuf2, 0x00, sizeof(rbuf2));
+
+	//rコマンドのメッセージを作成
+	sprintf(wbuf, "r 20%04x 16\r\n", (2048 + (map_add * 2)));
+
+	//送受信！
+	sendmessage(wbuf, rbuf);
+	if (DEBUG==1) debug_print(wbuf, rbuf);	// デバッグ表示
+
+	//受信結果を整形
+	if (rbuf[0] == '\0') rbuf[0] = ' ';	// 1文字目にNullが紛れ込んだときの対策
+
+	for (i = 0; rbuf[i] != '\0'; i++) {
+		if (rbuf[i] == '#') pos_sharp = i;
+	}
+
+	for (i = 0; rbuf[i + pos_sharp] != '\0'; i++) {
+		rbuf2[i] = rbuf[pos_sharp + i];
+	}
+
+	length = i;
+
+	// エラーチェック（返答の長さが適切かどうか）
+	if(length != 58) return -1;
+
+	// エラーチェック(送ったアドレスと受信したアドレスが一致しているか)
+	if(!((wbuf[2]==rbuf2[1]) && (wbuf[3]==rbuf2[2]) && (wbuf[4]==rbuf2[3]) && (wbuf[5]==rbuf2[4]) && (wbuf[6]==rbuf2[5]) && (wbuf[7]==rbuf2[6]))) return -1;
+
+	// ビッグエンディアン／リトルエンディアン並び替え
+	outputc0[0] = rbuf2[11];
+	outputc0[1] = rbuf2[12];
+	outputc0[2] = rbuf2[8];
+	outputc0[3] = rbuf2[9];
+	outputc0[4] = 0;
+
+	outputc1[0] = rbuf2[17];
+	outputc1[1] = rbuf2[18];
+	outputc1[2] = rbuf2[14];
+	outputc1[3] = rbuf2[15];
+	outputc1[4] = 0;
+
+	outputc2[0] = rbuf2[23];
+	outputc2[1] = rbuf2[24];
+	outputc2[2] = rbuf2[20];
+	outputc2[3] = rbuf2[21];
+	outputc2[4] = 0;
+
+	outputc3[0] = rbuf2[29];
+	outputc3[1] = rbuf2[30];
+	outputc3[2] = rbuf2[26];
+	outputc3[3] = rbuf2[27];
+	outputc3[4] = 0;
+
+	outputc4[0] = rbuf2[35];
+	outputc4[1] = rbuf2[36];
+	outputc4[2] = rbuf2[32];
+	outputc4[3] = rbuf2[33];
+	outputc4[4] = 0;
+
+	outputc5[0] = rbuf2[41];
+	outputc5[1] = rbuf2[42];
+	outputc5[2] = rbuf2[38];
+	outputc5[3] = rbuf2[39];
+	outputc5[4] = 0;
+
+	outputc6[0] = rbuf2[47];
+	outputc6[1] = rbuf2[48];
+	outputc6[2] = rbuf2[44];
+	outputc6[3] = rbuf2[45];
+	outputc6[4] = 0;
+
+	outputc7[0] = rbuf2[53];
+	outputc7[1] = rbuf2[54];
+	outputc7[2] = rbuf2[50];
+	outputc7[3] = rbuf2[51];
+	outputc7[4] = 0;
+
+	//16進数を数値に変換 -> short(2byte)に格納 (ここの処理の時間、一度チェックした方がいいかも)
+	value[0] = strtol(outputc0, NULL, 16);
+	value[1] = strtol(outputc1, NULL, 16);
+	value[2] = strtol(outputc2, NULL, 16);
+	value[3] = strtol(outputc3, NULL, 16);
+	value[4] = strtol(outputc4, NULL, 16);
+	value[5] = strtol(outputc5, NULL, 16);
+	value[6] = strtol(outputc6, NULL, 16);
+	value[7] = strtol(outputc7, NULL, 16);
+
+	return 0;
+}
+
+////////////////////////////////////////////////////////////////
+//	get_memmap関数
+//	メモリマップのアドレスを指定して、値を読む関数
+//	
+//	・引数
+//		HANDLE hCom	通信に使用するハンドル
+//		unsigned char map_add 読み出したいメモリマップのアドレス0～255
+//
+//	・戻り値
+//		メモリマップの値(符号付2バイト) ただし、return -32678はエラー
+////////////////////////////////////////////////////////////////
+
+short get_memmap(unsigned char map_add) {
+	char rbuf[BUFF_SZ], wbuf[BUFF_SZ], rbuf2[BUFF_SZ], outputc[4];
+	short value;
+	int pos_sharp = 0, i ,length;
+
+	//送受信メッセージバッファのクリア
+	memset(wbuf, '\0', sizeof(wbuf));
+	memset(rbuf, '\0', sizeof(rbuf));
+	memset(rbuf2, '\0', sizeof(rbuf2));
+
+	//rコマンドのメッセージを作成
+	sprintf(wbuf, "r 20%04x 02\r\n", (2048 + (map_add * 2)));
+
+	//送受信！
+	sendmessage(wbuf, rbuf);
+	if (DEBUG==1) debug_print(wbuf, rbuf);	// デバッグ表示
+
+	//受信結果を整形
+	if (rbuf[0] == '\0') rbuf[0] = ' ';	// 1文字目にNullが紛れ込んだときの対策
+
+	for (i = 0; rbuf[i] != '\0'; i++) {
+		if (rbuf[i] == '#') pos_sharp = i;
+	}
+
+	for (i = 0; rbuf[i + pos_sharp] != '\0'; i++) {
+		rbuf2[i] = rbuf[pos_sharp + i];
+	}
+
+	length = i;
+	// 返答の長さが適切かどうか
+	if(length != 16) return -32768;
+
+	// エラーチェック(送ったアドレスと受信したアドレスが一致しているか)
+	if(!((wbuf[2]==rbuf2[1]) && (wbuf[3]==rbuf2[2]) && (wbuf[4]==rbuf2[3]) && (wbuf[5]==rbuf2[4]) && (wbuf[6]==rbuf2[5]) && (wbuf[7]==rbuf2[6]))) return -32768;
+
+	outputc[0] = rbuf2[11];
+	outputc[1] = rbuf2[12];
+	outputc[2] = rbuf2[8];
+	outputc[3] = rbuf2[9];
+
+	//16進数を数値に変換 -> short(2byte)に格納
+	value = strtol(outputc, NULL, 16);
+
+	return value;
+}
+
+////////////////////////////////////////////////////////////////
+//	put_memmap関数
+//	メモリマップのアドレスを指定して、値を書き込む関数
+//	
+//	・引数
+//		HANDLE hCom 通信に使用するハンドル
+//		unsigned char map_add 書き込みたいメモリマップのアドレス0～255
+//		short value 書き込む値(符号付2バイト)
+//
+//	・戻り値(sendmessageそのまま)
+//		0	メッセージの送受信に成功
+//		1	メッセージの送信に失敗
+//		2	メッセージの受信に失敗
+//		3	メッセージの送受信に失敗
+////////////////////////////////////////////////////////////////
+
+int put_memmap(unsigned char map_add, short value) {
+	char rbuf[BUFF_SZ], wbuf[BUFF_SZ], value_1,value_2;
+	int err=0;
+
+	//送受信メッセージバッファのクリア
+	memset(wbuf, 0, sizeof(wbuf));
+	memset(rbuf, 0, sizeof(rbuf));
+
+	value_1 = value & 0x00FF;
+	value_2 = (value >> 8) & 0x00FF;
+
+	//wコマンドのメッセージを作成
+	sprintf(wbuf, "w 20%04x %02x %02x\r\n", (2048 + (map_add * 2)), value_1 & 0x000000FF, value_2 & 0x000000FF);
+
+	//送受信！
+	err = sendmessage(wbuf, rbuf);
+	if (DEBUG==1) debug_print(wbuf, rbuf);	// デバッグ表示
+
+	return err;
+}
+
+void read_vsrc(void){
+
+	int error=0;
+	short value1[8],value2[8];
+
+	error = get_memmap8(238,value1);
+	if (error==0){
+		r_mode = value1[0];	//238
+		r_voltage = value1[1];	//239
+		r_ctltype = value1[2];	//240
+	}else{
+		ROS_ERROR("get_memmap (238) error");
+	}
+	
+	error = get_memmap8(248,value2);
+	if (error==0){
+		r_map = value2[0];	//248 map
+		r_torque = value2[3];	//251 torque
+		r_idle = value2[5];	//253
+	}else{
+		ROS_ERROR("get_memmap (248) error");
+	}
+
+	ROS_INFO("mode:%d | voltage:%d[mV] | map:%d | r_torque:%d <=> w_torque:%d | idle:%d | Ctl_type:%d",r_mode,r_voltage,r_map,r_torque,w_torque,r_idle,r_ctltype);
+}
+
+void read_voltage_test(void){
+	r_voltage = get_memmap(239);
+	ROS_INFO("VOLTAGE=%d",r_voltage);
+}
+
+int write_vsrc(void){
+
+	int tmp1,tmp2,err;
+	short stk_rx,stk_ry,stk_lx,stk_ly;
+	char rbuf[BUFF_SZ], wbuf[BUFF_SZ];
+
+	err = 0;
+	err = put_memmap(251, w_torque);
+	if (err != 0) ROS_ERROR("w_torque : write error");
+
+	//送受信メッセージバッファのクリア
+	memset(wbuf, 0, sizeof(wbuf));
+	memset(rbuf, 0, sizeof(rbuf));
+	
+	// ボタンの書き込み変数作成
+	tmp1 = b_L2 + b_R2 * 2 + b_L1 * 4 + b_R1 * 8 + b_triangle * 16 + b_circle * 32+ b_cross * 64 + b_square * 128;
+	tmp2 = b_select + b_L3 * 2 + b_R3 * 4 + b_start * 8 + b_up * 16 + b_right * 32+ b_down * 64 + b_left * 128;
+
+	//VS-RC003コマンド生成
+	sprintf(wbuf,"w 2009e2 %02x %02x 00 00 00 00 %02x %02x %02x %02x %02x %02x %02x %02x\r\n", tmp1,tmp2,s_rx&0x00ff,(s_rx>>8)&0x00ff,s_ry&0x00ff,(s_ry>>8)&0x00ff,s_lx&0x00ff,(s_lx>>8)&0x00ff,s_ly&0x00ff,(s_ly>>8)&0x00ff);
+	//                     ==(241)== -242- -243- ==(244)== ==(245)== ==(246)== ==(247)==
+
+	//送受信！
+	if (w_torque==TRUE){				// トルクオンの時だけ送信
+		err = 0;
+		err = sendmessage(wbuf, rbuf);
+		if (DEBUG==1) debug_print(wbuf, rbuf);	// デバッグ表示
+		if (err != 0) ROS_ERROR("keypad info : write error");
+	}
+	return err;
+}
+
+void vsrc_ctl_callback(const ros2vsrc::VsrcControl& vsrcctl){
+
+	w_torque 	= vsrcctl.w_torque;
+
+	b_left		= vsrcctl.b_left;
+	b_down		= vsrcctl.b_down;
+	b_right		= vsrcctl.b_right;
+	b_up		= vsrcctl.b_up;
+	b_start		= vsrcctl.b_start;
+	b_R3		= vsrcctl.b_R3;
+	b_L3		= vsrcctl.b_L3;
+	b_select	= vsrcctl.b_select;
+	b_square	= vsrcctl.b_square;
+	b_cross		= vsrcctl.b_cross;
+	b_circle	= vsrcctl.b_circle;
+	b_triangle	= vsrcctl.b_triangle;
+	b_R1		= vsrcctl.b_R1;
+	b_L1		= vsrcctl.b_L1;
+	b_R2		= vsrcctl.b_R2;
+	b_L2		= vsrcctl.b_L2;
+	s_lx		= vsrcctl.s_lx;
+	s_ly		= vsrcctl.s_ly;
+	s_rx		= vsrcctl.s_rx;
+	s_ry		= vsrcctl.s_ry;
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "vsrc_connect");
-    ros::NodeHandle n;
-    ros::NodeHandle pn("~");
-    //param
-    pn.getParam("device_name", device_name);
-    ss0.set_name(device_name);
-    //Publisher
-    serial_pub = n.advertise<std_msgs::String>("vsrc_read", 10);
-    //timer
-    ros::Timer timer = n.createTimer(ros::Duration(0.01), timer_callback);
-    //Diagnostic
-    diagnostic_updater::Updater updater;
-    p_updater=&updater;
-    updater.setHardwareID("SerialPort");
-    updater.add("Connect", diagnostic0);
-    //Subscriber
-    ros::Subscriber serial_sub = n.subscribe("vsrc_write", 10, serial_callback); 
+	char rbuf[BUFF_SZ], wbuf[BUFF_SZ];
+	sensor_msgs::BatteryState battery_state;
+	ros2vsrc::VsrcState vsrc_state;
 
-    ros::spin();
-    ss0.ss_close();
-    return 0;
+	ros::init(argc, argv, "vsrc_connect");
+	ros::NodeHandle n;
+
+	// シリアルポート名をパラメータからセット
+	std::string serialdev;
+  	ros::NodeHandle pn("~");
+	pn.param<std::string>("serialdev", serialdev, SERIAL_PORT);
+	ROS_INFO("Set Serial Port : %s",serialdev.c_str());
+
+	//Publisher
+	ros::Publisher battery_pub = n.advertise<sensor_msgs::BatteryState>("battery_state",10);
+	ros::Publisher vsrc_pub = n.advertise<ros2vsrc::VsrcState>("vsrc_state",10);
+
+	//Subscriber
+	ros::Subscriber vsrc_sub = n.subscribe("vsrc_control", 10, vsrc_ctl_callback);
+
+	// シリアルオープン
+	fd1=open_serial(serialdev.c_str());
+	if(fd1<0){
+		ROS_ERROR("Serial Fail: cound not open %s", serialdev.c_str());
+		ros::shutdown();
+	}
+
+	ros::Rate loop_rate(100);
+
+	//送受信メッセージバッファのクリア
+	memset(wbuf, 0, sizeof(wbuf));
+	memset(rbuf, 0, sizeof(rbuf));
+
+	//VS-RC003の制御をコントローラからシリアル通信に切り替える
+	ROS_INFO("Start VS-RC003 Connect & Control");
+
+	sprintf(wbuf, "w 2009e0 00 00\r\n");
+	//メッセージの送受信。通信が成功しなかったらプログラムを終了 (VS-RC003初回起動時は失敗することがある)
+	if (sendmessage(wbuf, rbuf)) {
+		ROS_ERROR("Cannot send massage to VS-RC003");
+		close(fd1); // ポートクローズ
+		return 0;
+	}
+
+	while (ros::ok()){	// ここでメインループ！
+		read_vsrc();				// VS-RC003からメモリ読み出し
+
+		battery_state.voltage = (float)(r_voltage/1000.0);
+		battery_pub.publish(battery_state);	// バッテリー情報を配信
+
+		vsrc_state.r_mode = r_mode;
+		vsrc_state.r_map = r_map;
+		vsrc_state.r_ctltype = r_ctltype;
+		vsrc_state.r_torque = r_torque;
+		vsrc_state.r_idle = r_idle;
+		vsrc_pub.publish(vsrc_state);		// センサー情報を配信
+
+		write_vsrc();				// VS-RC003のメモリ書き込み
+
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
 }
